3c29048cd8ed97979cf488489d563769fc22a666
mcberry23 <mattcastle38@gmail.com>
2020-11-25 21:11:08 -0600

    Added ROS project with IMU integration.  Altimeter project is not operational yet

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/gavlab/ros-hab-dcs/catkin_ws/src
+# Build directory: /home/gavlab/ros-hab-dcs/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("bb_altimeter")
+subdirs("ros-imu-bno055/imu_bno055")
---
+++ catkin_ws/build/bb_altimeter/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gavlab/ros-hab-dcs/catkin_ws/src/bb_altimeter
+# Build directory: /home/gavlab/ros-hab-dcs/catkin_ws/build/bb_altimeter
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
