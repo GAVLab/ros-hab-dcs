b9dc9cb77ee7795e553e2f9b46aa2d6accb67d3c
mcberry23 <mattcastle38@gmail.com>
2020-12-14 21:32:36 -0600

    Added IMU and raspicam_node.  Various launch file and documentation improvements

---
+++ catkin_ws/build/raspicam_node/CMakeFiles/raspicam_node_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/gavlab/ros-hab-dcs/catkin_ws/devel/include/raspicam_node/CameraConfig.h"
+  "/home/gavlab/ros-hab-dcs/catkin_ws/devel/lib/python3/dist-packages/raspicam_node/cfg/CameraConfig.py"
+  "/home/gavlab/ros-hab-dcs/catkin_ws/devel/share/raspicam_node/docs/CameraConfig-usage.dox"
+  "/home/gavlab/ros-hab-dcs/catkin_ws/devel/share/raspicam_node/docs/CameraConfig.dox"
+  "/home/gavlab/ros-hab-dcs/catkin_ws/devel/share/raspicam_node/docs/CameraConfig.wikidoc"
+  "CMakeFiles/raspicam_node_gencfg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/raspicam_node_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/raspicam_node/CMakeFiles/raspicam_node_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "/home/gavlab/ros-hab-dcs/catkin_ws/devel/include/raspicam_node/MotionVectors.h"
+  "CMakeFiles/raspicam_node_generate_messages_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/raspicam_node_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
