af3b4a2e49c293099ffee07685df7e8218a5df04
MatthewCastleberry <mtc0030@auburn.edu>
2021-02-09 15:04:26 -0600

    Fixed APRS coordinate conversions

--- aprs/parse-aprs.py
+++ aprs/parse-aprs.py
@@ -5,20 +5,22 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import re
 
+# Parameters
+region = 'auburn_macon'
+unit_system = 'metric' # 'us' or 'metric'
 
 # Map bounding box
-range = 'auburn_macon'
-if range == 'auburn':
+if region == 'auburn':
     BBox = ((-85.6110,-84.9712,32.4864,32.7197))
     map_img = plt.imread("map_auburn.png")
-elif range == 'auburn_macon':
+elif region == 'auburn_macon':
     BBox = ((-85.6514,-82.8561,32.1011,33.1847))
     map_img = plt.imread("map_auburn_macon.png")
-elif range == 'macon':
+elif region == 'macon':
     BBox = ((-83.7592,-82.4369,32.7313,33.5575))
     map_img = plt.imread("map_macon.png")
 else:
-    print("Unknown range")
+    print("Unknown region")
 
 # Using readlines()
 file1 = open('aprs-log.txt', 'r')
@@ -39,13 +41,20 @@ for line in Lines:
     timestamps[count] = packet['timestamp']
     latitudes[count] = packet['latitude']
     longitudes[count] = packet['longitude']
-    altitudes[count] = packet['altitude']
-    speeds[count] = packet['speed']
     courses[count] = packet['course']
     voltages[count] = int(re.findall(r'V=(\w+)', packet['comment'])[0])/1000
     dt = datetime.utcfromtimestamp(timestamps[count]).strftime('%Y-%m-%d %H:%M:%S')
-    print("%s:\t[Lat,Lon,Alt(m)] = %.6f,%.6f,%.2f\tCourse = %d\tSpeed = %.2f\tV = %.2f\t" % 
-    (dt,latitudes[count],longitudes[count],altitudes[count],courses[count],speeds[count],voltages[count]))
+    raw_speed = packet['speed'] # speed in km/h
+    if unit_system == 'metric':
+        speeds[count] = packet['speed']*0.277778 # speed in m/s
+        altitudes[count] = packet['altitude'] # altitude in m
+        print(u"%s:\t[Lat,Lon,Alt(m)] = %.6f,%.6f,%.2f\tCourse = %d\xb0\tSpeed = %.2fm/s\tV = %.3f\t" % 
+        (dt,latitudes[count],longitudes[count],altitudes[count],courses[count],speeds[count],voltages[count]))
+    else:
+        speeds[count] = packet['speed']*0.621371689334 # speed in mph
+        altitudes[count] = packet['altitude'] * 3.28084 # altitude in ft
+        print(u"%s:\t[Lat,Lon,Alt(ft)] = %.6f,%.6f,%.2f\tCourse = %d\xb0\tSpeed = %.2fmph\tV = %.3f\t" % 
+        (dt,latitudes[count],longitudes[count],altitudes[count],courses[count],speeds[count],voltages[count]))
     count += 1
 
 # Plot results    
@@ -60,20 +69,26 @@ ax.imshow(map_img,zorder=0,extent = BBox, aspect='equal')
 
 fig,ax = plt.subplots(2,2)
 
-ax[0,0].plot(altitudes)
-ax[0,0].set(xlabel="Index",ylabel="Altitude (m)")
+ax[0,0].plot(timestamps,altitudes,marker='o')
+if unit_system == "metric":
+    ax[0,0].set(xlabel="Time",ylabel="Altitude (m)")
+else:
+    ax[0,0].set(xlabel="Time",ylabel="Altitude (ft)")
 ax[0,0].set_title("Altitude")
 
-ax[1,0].plot(courses)
-ax[1,0].set(xlabel="Index",ylabel="Course")
+ax[1,0].plot(timestamps,courses,marker='o')
+ax[1,0].set(xlabel="Time",ylabel="Course")
 ax[1,0].set_title("GPS Course")
 
-ax[0,1].plot(speeds)
-ax[0,1].set(xlabel="Index",ylabel="Speed (m/s)")
+ax[0,1].plot(timestamps,speeds,marker='o')
+if unit_system == "metric":
+    ax[0,1].set(xlabel="Time",ylabel="Speed (m/s)")
+else:
+    ax[0,1].set(xlabel="Time",ylabel="Speed (mph)")
 ax[0,1].set_title("GPS Speed")
 
-ax[1,1].plot(voltages)
-ax[1,1].set(xlabel="Index",ylabel="Voltage (V)")
+ax[1,1].plot(timestamps,voltages,marker='o')
+ax[1,1].set(xlabel="Time",ylabel="Voltage (V)")
 ax[1,1].set_title("Voltage Levels")
 
 plt.tight_layout()
