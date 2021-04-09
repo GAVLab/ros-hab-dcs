4e56dca44ccaa71445d2ceb091782b13b9524437
mcberry23 <mtc0030@auburn.edu>
2021-04-09 15:02:19 -0500

    Fully integrated ros arduino ins

--- launches/record_full.sh
+++ launches/record_full.sh
@@ -8,7 +8,8 @@ echo "Recording...."
 rosbag record -o ~/bagfiles/flight --split --duration=5m subset \
 /rosout /rousout_agg \
 /down_cam/camera_info /down_cam/image/compressed /down_cam/parameter_descriptions \
-/icm20948/mag /icm20948/raw \
+/hts221/humidity /hts221/temperature /lps22hb/pressure \
+/icm20948/data /bno080/data \
 /mpl3115a2/altitude /mpl3115a2/pressure /mpl3115a2/temperature \
 /ublox/aideph /ublox/fix /ublox/fix_velocity /ublox/monhw /ublox/navclock /ublox/navpvt /ublox/navsat /ublox/navstatus \
 /rpi/cpu_temp \
